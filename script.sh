a5137d6649559358f77397296164b273c19ee6fd
ben <benmotykax@gmail.com>
2022-11-25 23:50:58 +0100

    added checking for empty file

--- script.sh
+++ script.sh
@@ -1,25 +1,36 @@
 #! /bin/bash
 
+help="Usage: ./script.sh [FILE]\nTry'./script --help' for more information.\n"
+
 if [ $# -eq 0 ]
-    then
-        printf "Usage: ./script.sh [FILE]\nTry'./script --help' for more information.\n"
-        exit 1
-fi 
+then
+    printf "$help"
+    exit 1
+fi
 
 if [[ $* == *--help* ]]
-    then
-        printf "Typed help\n"
-        exit 1
+then
+    printf "Typed help\n"
+    exit 1
 fi
 
 envs=`cat $1`
+
+if [ -z "$envs" ]
+then
+    printf "ERROR: Environment file empty!\n"
+    exit 2
+fi
+
 joinedEnvs=""
 
 for value in $envs
-    do
+do
     joinedEnvs+="$value,"
 done
 
-aws elasticbeanstalk update-environment --environment-name my-env --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=PARAM1,Value=ParamValue
+joinedEnvs=${joinedEnvs::-1}
 
-echo $joinedEnvs
+# aws elasticbeanstalk update-environment --environment-name my-env --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=PARAM1,Value=ParamValue
+
+printf "aws elasticbeanstalk update-environment --environment-name my-env --option-settings Namespace=aws:elasticbeanstalk:application:environment,$joinedEnvs"
