e2b9e4050ce1a8b389e2862b06b111feeb319902
Ben <benmotykax@gmail.com>
2022-11-26 22:28:38 +0100

    added confirmation prompt, added env variable

--- script.sh
+++ script.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
-help="Usage: ./script.sh [FILE]\nTry'./script --help' for more information.\n"
-
-if [ $# -eq 0 ]
-then
-    printf "$help"
-    exit 1
-fi
+shortHelp="Usage: ./script.sh [ENVIRONMENT_NAME] [FILE]\nTry'./script --help' for more information.\n"
+longHelp="Long help \n"
 
 if [[ $* == *--help* ]]
 then
-    printf "Typed help\n"
+    printf "$longHelp"
     exit 1
 fi
 
-envs=`cat $1`
+if [ $# -lt 2 ]
+then
+    printf "$shortHelp"
+    exit 1
+fi
+
+environment=$1
+envs_file=$2
+
+envs=`cat $envs_file`
 
 if [ -z "$envs" ]
 then
@@ -33,4 +37,15 @@ joinedEnvs=${joinedEnvs::-1}
 
 # aws elasticbeanstalk update-environment --environment-name my-env --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=PARAM1,Value=ParamValue
 
-printf "aws elasticbeanstalk update-environment --environment-name my-env --option-settings Namespace=aws:elasticbeanstalk:application:environment,$joinedEnvs"
+read -r -p "Are you sure to update your environment name: $environment with envs from file: $envs_file? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        printf "aws elasticbeanstalk update-environment --environment-name $environment --option-settings Namespace=aws:elasticbeanstalk:application:environment,$joinedEnvs"
+        ;;
+    *)
+        printf "Operation aborted\n"
+        exit 1
+        ;;
+esac
+
+
