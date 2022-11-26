36f724121b456cb03686f72b556a58882587a81f
Ben <benmotykax@gmail.com>
2022-11-26 23:04:57 +0100

    added auto approve flag, checking for non existent file, changed naming convention of variables and functions

--- script.sh
+++ script.sh
@@ -1,22 +1,34 @@
 #! /bin/bash
 
-shortHelp="Usage: ./script.sh [ENVIRONMENT_NAME] [FILE]\nTry'./script --help' for more information.\n"
-longHelp="Long help \n"
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+short_help="Usage: ./script.sh [EB_ENVIRONMENT_NAME] [FILE]\nTry'./script --help' for more information.\n"
+long_help="Long help \n"
+
+update_environment () {
+        printf "aws elasticbeanstalk update-environment --environment-name $eb_environment_name --option-settings Namespace=aws:elasticbeanstalk:application:environment,$joined_envs"
+}
 
 if [[ $* == *--help* ]]
 then
-    printf "$longHelp"
+    printf "$long_help"
     exit 1
 fi
 
 if [ $# -lt 2 ]
 then
-    printf "$shortHelp"
+    printf "$short_help"
     exit 1
 fi
 
-environment=$1
+eb_environment_name=$1
 envs_file=$2
+
+if [ ! -f "$envs_file" ]; then
+    printf "ERROR: File doesn't exist!\n"
+    exit 2
+fi
 
 envs=`cat $envs_file`
 
@@ -26,26 +38,31 @@ then
     exit 2
 fi
 
-joinedEnvs=""
+joined_envs=""
 
 for value in $envs
 do
-    joinedEnvs+="$value,"
+    joined_envs+="$value,"
 done
 
-joinedEnvs=${joinedEnvs::-1}
+joined_envs=${joined_envs::-1}
 
-# aws elasticbeanstalk update-environment --environment-name my-env --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=PARAM1,Value=ParamValue
+if [[ $* == *--auto-approve* ]]
+then
+    update_environment
+    else 
+        read -r -p "Are you sure to update your environment name:${bold} $eb_environment_name ${normal}with envs from file:${bold} $envs_file? ${normal}[y/N] " response
+        case "$response" in
+            [yY][eE][sS]|[yY]) 
+                    update_environment
+                ;;
+            *)
+                printf "Operation aborted\n"
+                exit 1
+                ;;
+        esac
+fi
 
-read -r -p "Are you sure to update your environment name: $environment with envs from file: $envs_file? [y/N] " response
-case "$response" in
-    [yY][eE][sS]|[yY]) 
-        printf "aws elasticbeanstalk update-environment --environment-name $environment --option-settings Namespace=aws:elasticbeanstalk:application:environment,$joinedEnvs"
-        ;;
-    *)
-        printf "Operation aborted\n"
-        exit 1
-        ;;
-esac
+
 
 
